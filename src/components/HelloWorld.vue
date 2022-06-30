02fc558aaf168de8fdc9b5834d80fb650c8ce940
Shake1999 <northstarsh@gmail.com>
2022-06-30 00:28:11 -0700

    .

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -8,9 +8,7 @@ defineProps<{
   <div class="greetings">
     <h1 class="green">{{ msg }}</h1>
     <h3>
-      You’ve successfully created a project with
-      <a target="_blank" href="https://vitejs.dev/">Vite</a> +
-      <a target="_blank" href="https://vuejs.org/">Vue 3</a>. What's next?
+      TESTING COMPLETE!!!
     </h3>
   </div>
 </template>
