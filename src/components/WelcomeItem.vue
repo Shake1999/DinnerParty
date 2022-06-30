6b3366b4436c19836771ca67e7cc24dbbf2f47a5
Shake1999 <northstarsh@gmail.com>
2022-06-30 16:36:35 -0700

    .

--- src/components/WelcomeItem.vue
+++ src/components/WelcomeItem.vue
@@ -1,8 +1,5 @@
 <template>
   <div class="item">
-    <i>
-      <slot name="icon"></slot>
-    </i>
     <div class="details">
       <h3>
         <slot name="heading"></slot>
@@ -23,16 +20,6 @@
   margin-left: 1rem;
 }
 
-i {
-  display: flex;
-  place-items: center;
-  place-content: center;
-  width: 32px;
-  height: 32px;
-
-  color: var(--color-text);
-}
-
 h3 {
   font-size: 1.2rem;
   font-weight: 500;
@@ -44,17 +31,6 @@ h3 {
   .item {
     margin-top: 0;
     padding: 0.4rem 0 1rem calc(var(--section-gap) / 2);
-  }
-
-  i {
-    top: calc(50% - 25px);
-    left: -26px;
-    position: absolute;
-    border: 1px solid var(--color-border);
-    background: var(--color-background);
-    border-radius: 8px;
-    width: 50px;
-    height: 50px;
   }
 
   .item:before {
