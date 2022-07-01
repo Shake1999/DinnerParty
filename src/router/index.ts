9bfe66bf303640677f3eeb394adf0a5916c47455
Shake1999 <northstarsh@gmail.com>
2022-06-30 17:38:18 -0700

    .

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,10 +2,14 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory('/JustTesting/'),
   routes: [
     {
       path: '/',
+      redirect: {name: 'home'}
+    },
+    {
+      path: '/JustTesting/',
       name: 'home',
       component: HomeView
     },
